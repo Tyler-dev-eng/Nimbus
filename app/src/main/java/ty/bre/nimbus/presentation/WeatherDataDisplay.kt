05c555eba5a50db2ae47867d357c3f17cb9bf7eb
tyleravochoc <tyler@avochoc.com>
2025-01-30 17:16:05 +0200

    Small changes

--- app/src/main/java/ty/bre/nimbus/presentation/WeatherDataDisplay.kt
+++ app/src/main/java/ty/bre/nimbus/presentation/WeatherDataDisplay.kt
@@ -1,6 +1,5 @@
 package ty.bre.nimbus.presentation
 
-import android.widget.Space
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
